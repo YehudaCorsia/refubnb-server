7bbb33f3a69b88fd4ed3378b4cccab03b70254a4
YehudaCorsia <Yehudacorsia@gmail.com>
2022-05-14 22:47:21 +0300

    Add files via upload

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,3 @@
-// module.exports = {
-// 	dbURL: `mongodb+srv://dandan:${process.env.MONGO_PASS}@RefuBNB.jk6co.mongodb.net/RefuBNB_db?retryWrites=true&w=majority`,
-// }
 module.exports = {
-	dbURL: 'mongodb://localhost:27017',
+	dbURL: `mongodb+srv://refubnb:${process.env.MONGO_PASS}@cluster0.cxiwr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
 }
